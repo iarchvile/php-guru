9b95c4f47b45f5162b63607c5a561db7edbe85e2
Konstantin Kondratenko <info@0x25.ru>
2020-11-21 23:14:13 +0300

    Полнотекстовый поиск scout Algolia

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    //factory(App\User::class, 100)->create();
+
+    /*\App\User::create([
+        'name' => 'Константин',
+        'email' => 'archvile.bk@ya.ru',
+        'password' => 'foo'
+    ]);*/
+
+
+    //dd(\App\User::search('Mr.')->get()->pluck('name'));
+    dd(\App\User::search('Dr.')->get()->pluck('name'));
+
+
     return view('welcome');
 });
