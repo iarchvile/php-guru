5fedc7ea9c988dbce76fba74b182c2980604d277
Konstantin Kondratenko <info@0x25.ru>
2020-11-27 17:58:08 +0300

    fixup! Понлнотекстовый поиск scout Alg

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,30 @@ Route::get('/', function () {
 
     //factory(App\User::class, 100)->create();
 
-    /*\App\User::create([
-        'name' => 'Константин',
-        'email' => 'archvile.bk@ya.ru',
-        'password' => 'foo'
-    ]);*/
+    /*$names = [
+        'Новый Год начинается с сюрпризов!',
+        'Праздник чистого добра!',
+        'Время чистых чудес!',
+        'Новый Год в чистоте!',
+        'Стирайте старое, получайте новое!',
+        'Новогодние чудеса — это просто!',
+        'Подари Новогоднее чудо!',
+        'Новый год — время добрый дел!',
+        'Новогодние чудеса от чистого сердца!',
+        'Стань частью новогоднего чуда!',
+    ];
+
+    foreach ($names as $key => $name) {
+        \App\User::create([
+            'name' => $name,
+            'email' => $key.'zarchvile.bk@ya.ru',
+            'password' => 'foo'
+        ]);
+    }*/
 
 
     //dd(\App\User::search('Mr.')->get()->pluck('name'));
-    dd(\App\User::search('Dr.')->get()->pluck('name'));
+    dd(\App\User::search('Константин')->get()->pluck('name'));
 
 
     return view('welcome');
